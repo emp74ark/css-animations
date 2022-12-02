dd798164ed99b959d33bd9d534bc3e5d8dc785d0
emp74ark <andrey.yurkovsky@gmail.com>
2022-12-02 23:40:01 +0100

    feat: simple paralax effect for home

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,7 +1,28 @@
 import {FC} from 'react';
+import style from './home.module.css';
 
 export const HomeComponent: FC = () => {
   return(
-      <h1>Home</h1>
+      <>
+        <section className={style.section} id="home">
+          <h1 className={style.greeting}>Welcome</h1>
+          <a href="#skills">
+            <div className={style.arrow_up} />
+          </a>
+        </section>
+        <section className={style.section} id="skills">
+          <a href="#">
+            <div className={style.arrow_down} />
+          </a>
+          <ul className={style.skills}>
+            <li>HTML</li>
+            <li>CSS/SCSS</li>
+            <li>JavaScript</li>
+            <li>Typescript</li>
+            <li>Angular</li>
+            <li>React</li>
+          </ul>
+        </section>
+      </>
   );
 }
